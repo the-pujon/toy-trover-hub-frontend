4d7be565412332b4309f8a88a12e7a7c40973368
Pujon Das Auvi <auvipujondas@gmail.com>
2024-01-27 23:39:03 +0600

    banner picture style done

--- src/Pages/Customer/Home/Banner/Banner.jsx
+++ src/Pages/Customer/Home/Banner/Banner.jsx
@@ -52,28 +52,73 @@ import React from "react";
 import img from "../../../../assets/about.jpg";
 import "./Banner.scss";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+//import './styles.css';
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
 const Banner = () => {
   return (
-    <div className="wrapper mx-auto max-h-screen" >
-      <aside class="responsive-banner second">
-      <div className="bg-black/5 flex items-center justify-center w-[1000px] rounded-full h-[1000px]" >
-             <img src={img} className="w-[900px] rounded-full h-[900px] -z-10" />
-      </div>
-        <div class="container-envelope">
+    <div className="wrapper mx-auto max-h-screen !pt-20">
+      <aside class="responsive-banner second flex justify-between w-full bg-">
+        <div className="flex-1">
+          <div>
+            Welcome to toy trover hub
+          </div>
+        </div>
+        <div className="flex-1">
+
+         <div className=" bg-black/5 flex items-center justify-center w-[950px] border-rad h-[950px]">
+            <div className=" bg-black/5 flex items-center justify-center w-[850px] border-rad h-[850px]">
+              <Swiper
+                pagination={true}
+                modules={[Pagination]}
+                className="mySwiper w-[750px] bg-black/5  border-rad h-[750px] flex items-center justify-center"
+              >
+                <SwiperSlide className="swiper-slide">
+                  <img
+                    src={img}
+                    className="w-[700px] border-rad h-[700px] "
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src={img}
+                    className="w-[700px] border-rad h-[700px] "
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src={img}
+                    className="w-[700px] border-rad h-[700px] "
+                  />
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+
+        </div>
+        {/*<div class="container-envelope">
           <svg class="circle-a" height="200" width="200">
             <circle cx="100" cy="100" r="100" />
           </svg>
           <svg class="circle-b" height="200" width="200">
             <circle cx="80" cy="80" r="80" />
           </svg>
-          {/*<svg class="circle-c" height="1000" width="1000">
+          <svg class="circle-c" height="1000" width="1000">
             <circle cx="400" cy="400" r="400" />
-          </svg>*/}
-          {/*<svg class="cirle-d" height="60" width="60">
+          </svg>
+          <svg class="cirle-d" height="60" width="60">
             <circle cx="30" cy="30" r="30" />
-          </svg>*/}
-          {/*<img src={img} />*/}
-          {/*<div class="col-xs-12">
+          </svg>
+          <img src={img} />
+          <div class="col-xs-12">
             <p>Live Sites using our WordPress Themes</p>
             <a
               target="_blank"
@@ -82,10 +127,9 @@ const Banner = () => {
             >
               Get inspired
             </a>
-          </div>*/}
-        </div>
+          </div>
+        </div>*/}
       </aside>
-
     </div>
   );
 };
