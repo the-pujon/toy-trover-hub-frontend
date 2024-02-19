4332f7d0e8e4882d5f124c7b798e83ab2cced13a
Pujon Das Auvi <auvipujondas@gmail.com>
2024-02-20 00:54:22 +0600

    user image url updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -136,7 +136,7 @@ const Navbar = () => {
                     className="btn btn-ghost btn-circle avatar"
                   >
                     <div className="w-10 rounded-full">
-                      <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                      <img src={loggedUser?.photoURL}/>
                     </div>
                   </label>
                   <ul
